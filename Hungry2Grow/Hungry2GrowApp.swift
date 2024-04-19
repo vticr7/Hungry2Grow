6922ff3dbc37affd9ebf9d3b7b06ad4a67e69f50
vticr7 <vti9227@gmail.com>
2024-04-19 19:32:03 +0530

    Initial Commit

---
+++ Hungry2Grow/Hungry2GrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hungry2GrowApp.swift
+//  Hungry2Grow
+//
+//  Created by Vaibhav  Tiwary on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Hungry2GrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
